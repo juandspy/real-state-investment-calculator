c89fab1d72193c72bc4e1830e32f7cdef8c38e87
Juan Diaz Suarez <jdiazsua@redhat.com>
2022-01-09 13:28:52 +0100

    add cost by taxes

---
+++ cost_calculator.py
@@ -0,0 +1,52 @@
+from data import TYPE_NAME, PERCENTAGE_NAME, QUANTITY_NAME
+
+
+class BreakdownCosts:
+    def __init__(self):
+        self.data = {
+            TYPE_NAME: [],
+            PERCENTAGE_NAME: [],
+            QUANTITY_NAME: []
+        }
+
+    def add_cost(self, _type, percentage, quantity):
+        self.data[TYPE_NAME] += [_type]
+        self.data[PERCENTAGE_NAME] += [percentage]
+        self.data[QUANTITY_NAME] += [quantity]
+
+
+def calculate_taxes(bc: BreakdownCosts(), initial_cost, iaj, itp, iva):
+    """Returns a dictionary with the cost of each tax."""
+    tax_names = [
+        "Impuesto de Actos Jurídicos",
+        "Impuesto de Transferencia del Patrimonio",
+        "IVA"
+    ]
+    tax_values = [iaj, itp, iva]
+    for i in range(len(tax_names)):
+        bc.add_cost(
+            _type=tax_names[i],
+            percentage=f"{tax_values[i]*100:.1f}%",
+            quantity=initial_cost * tax_values[i]
+        )
+    return bc
+
+
+def calculate_maintenance(bc: BreakdownCosts()):
+    """To be done."""
+    return bc
+
+
+def calculate_risks(bc: BreakdownCosts()):
+    """To be done."""
+    return bc
+
+
+def calculate_total(bc: BreakdownCosts()):
+    """Aggregates all the costs in the dictionary"""
+    bc.add_cost(
+        _type="Total",
+        percentage="",
+        quantity=sum(bc.data[QUANTITY_NAME])
+    )
+    return bc
---
+++ data.py
@@ -0,0 +1,37 @@
+
+TYPE_NAME = "Tipo"
+PERCENTAGE_NAME = "Porcentaje"
+QUANTITY_NAME = "Cantidad"
+
+
+class Taxes:
+    def __init__(self, iaj, itp, iva):
+        self.iaj = iaj/100
+        self.itp = itp/100
+        self.iva = iva/100
+
+
+IVA = 10
+
+TAXES_BY_PROVINCES = {
+    "Seleccione una provincia": None,
+    "Andalucía":            Taxes(1.5, 8., IVA),
+    "Aragón":               Taxes(1.5, 8., IVA),
+    "Asturias":             Taxes(1.2, 8., IVA),
+    "Baleares":             Taxes(1.2, 8., IVA),
+    "Canarias":             Taxes(1.0, 6.5, 6.5),
+    "Cantabria":            Taxes(1.5, 10., IVA),
+    "Castilla-La Mancha":   Taxes(1.25, 9., IVA),
+    "Castilla y León":      Taxes(1.5, 8., IVA),
+    "Cataluña":             Taxes(1.5, 10., IVA),
+    "Ceuta":                Taxes(0.5, 6., IVA),
+    "Comunidad Valenciana": Taxes(1.5, 10., IVA),
+    "Extremadura":          Taxes(1.5, 8., IVA),
+    "Galicia":              Taxes(1.5, 10., IVA),
+    "La Rioja":             Taxes(1.0, 7., IVA),
+    "Comunidad de Madrid":  Taxes(0.75, 6., IVA),
+    "Melilla":              Taxes(0.5, 6., IVA),
+    "Murcia":               Taxes(1.5, 8., IVA),
+    "Navarra":              Taxes(0.5, 6., IVA),
+    "País Vasco":           Taxes(0.5, 7., IVA),
+}
